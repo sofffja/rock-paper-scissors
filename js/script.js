85a3f183763aebc91d84139c838af3370b976e46
sofffja <sofi.efron@gmail.com>
2023-12-12 18:46:24 -0300

    Create getComputerChoice function to return either rock, paper or scissors randomly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('hi!')
+function getComputerChoice() {
+    //return either rock, paper or scissors
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch (computerChoice) {
+        case 1:
+            return 'Rock';
+        break;
+        case 2:
+            return 'Paper';
+        break;
+        case 3:
+            return 'Scissors';
+        break;
+        default:
+            return 'Error, computer made a bad choice';
+        break;
+    }
+}
+
+console.log(getComputerChoice())
+
+//get player selection
+//compare computer choice to player selection
+//declare winner of the round
