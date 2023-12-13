886bcf9e776aa6938c9e55d4cdc71352364a4178
sofffja <sofi.efron@gmail.com>
2023-12-13 09:46:02 -0300

    Add playRound() to compare selections and declare winner

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let playerSelection = prompt('choose your fighter: rock, paper or scissors', '')
+alert(playRound(playerSelection, getComputerChoice()))
+
+
 function getComputerChoice() {
     //return either rock, paper or scissors
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -12,14 +16,30 @@ function getComputerChoice() {
         case 3:
             return 'Scissors';
         break;
-        default:
-            return 'Error, computer made a bad choice';
-        break;
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    //compare computer choice to player selection
+    //declare winner of the round
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
 
-//get player selection
-//compare computer choice to player selection
-//declare winner of the round
+    if (playerSelection === computerSelection) {
+        return `Ties! ${playerSelection} equals ${computerSelection.toLowerCase()}`
+    } else if (
+        playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+        playerSelection === 'Paper' && computerSelection === 'Rock' ||
+        playerSelection === 'Scissors' && computerSelection === 'Paper'
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection.toLowerCase()}`
+    } else {
+        return `You loose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+    }
+}
+
+function capitalize(text) {
+    return (
+        text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+    )
+}
