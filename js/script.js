9e758b09349c8ce0068e37f5d914c5651c48bd34
sofffja <sofi.efron@gmail.com>
2023-12-18 10:25:05 -0300

    Clean code and comments

--- js/script.js
+++ js/script.js
@@ -1,77 +1,70 @@
-console.log(game())
+console.log(game());
 
 
 function getComputerChoice() {
-    //return either rock, paper or scissors
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+	let computerChoice = Math.floor(Math.random() * 3);
 
-    switch (computerChoice) {
-        case 1:
-            return 'Rock';
-        break;
-        case 2:
-            return 'Paper';
-        break;
-        case 3:
-            return 'Scissors';
-        break;
-    }
-}
+	switch (computerChoice) {
+		case 0:
+			return 'Rock';
+		case 1:
+			return 'Paper';
+		case 2:
+			return 'Scissors';
+	};
+};
 
 function playRound(playerSelection, computerSelection) {
-    //compare computer choice to player selection
-    //declare winner of the round
-    if (playerSelection === computerSelection) {
-        return 'ties';
-    } else if (
-        playerSelection === 'Rock' && computerSelection === 'Scissors' ||
-        playerSelection === 'Paper' && computerSelection === 'Rock' ||
-        playerSelection === 'Scissors' && computerSelection === 'Paper'
-    ) {
-        return 'player';
-    } else {
-        return 'computer';
-    }
-}
+	if (playerSelection === computerSelection) {
+		return 'ties';
+	} else if (
+	playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+	playerSelection === 'Paper' && computerSelection === 'Rock' ||
+	playerSelection === 'Scissors' && computerSelection === 'Paper'
+	) {
+		return 'player';
+	} else {
+		return 'computer';
+	}
+};
 
 function game() {
-    let playerCounter = 0;
-    let computerCounter = 0;
-    let roundWinner;
+	let playerCounter = 0;
+	let computerCounter = 0;
+	let roundWinner;
 
-    for (i = 1; i <= 5; i++) {
-        console.log('round ' + i);
+	for (i = 1; i <= 5; i++) {
+		let playerSelection = capitalize(prompt('choose your fighter: rock, paper or scissors', ''));
+		let computerSelection = getComputerChoice();
+		roundWinner = playRound(playerSelection, computerSelection);
 
-        let playerSelection = capitalize(prompt('choose your fighter: rock, paper or scissors', ''))
-        let computerSelection = getComputerChoice();
-        roundWinner = playRound(playerSelection, computerSelection);
+		switch (roundWinner) {
+			case 'ties':
+				console.log(`Ties! ${playerSelection} equals ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
+				i--;
+			break;
 
-        switch (roundWinner) {
-            case 'ties':
-                console.log(`Ties! ${playerSelection} equals ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
-                i--;
-            break;
-    
-            case 'player':
-                playerCounter++;
-                console.log(`You win! ${playerSelection} beats ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`)
-            break;
-    
-            case 'computer':
-                computerCounter++;
-                console.log(`You loose! ${computerSelection} beats ${playerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`)
-        }
-    }
+			case 'player':
+				playerCounter++;
+				console.log(`You win! ${playerSelection} beats ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
+			break;
 
-    if (playerCounter > computerCounter) {
-        return 'YOU WIN THE GAME';
-    } else {
-        return 'MACHINE WINS THE GAME';
-    }
-}
+			case 'computer':
+				computerCounter++;
+				console.log(`You loose! ${computerSelection} beats ${playerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
+			break;
+		};
+	};
+
+	if (playerCounter > computerCounter) {
+		return 'YOU WIN THE GAME';
+	} else {
+		return 'MACHINE WINS THE GAME';
+	};
+};
 
 function capitalize(text) {
-    return (
-        text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
-    )
-}
+	return (
+		text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+	)
+};
