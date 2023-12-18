42ed8c19f0417d3fcd808021e6eb9478570d9a8e
sofffja <sofi.efron@gmail.com>
2023-12-18 12:59:46 -0300

    Add basic ui and update script to work with buttons input

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-console.log(game());
+let roundCounter = 0;
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+
+let playerButtons = document.querySelector('.player-buttons')
+let resultsDiv = document.querySelector('.results')
+
+playerButtons.addEventListener('click', (event) => {
+	playerSelection = event.target.id;
+
+	roundCounter < 5 ?
+		game(playerSelection) :
+		resultsDiv.textContent = endGame();
+});
 
 
-function getComputerChoice() {
-	let computerChoice = Math.floor(Math.random() * 3);
-
-	switch (computerChoice) {
-		case 0:
-			return 'Rock';
-		case 1:
-			return 'Paper';
-		case 2:
-			return 'Scissors';
-	};
-};
 
 function playRound(playerSelection, computerSelection) {
+	playerSelection = capitalize(playerSelection);
+
 	if (playerSelection === computerSelection) {
 		return 'ties';
 	} else if (
@@ -28,38 +32,57 @@ function playRound(playerSelection, computerSelection) {
 	}
 };
 
-function game() {
-	let playerCounter = 0;
-	let computerCounter = 0;
+function game(playerSelection) {
 	let roundWinner;
 
-	for (i = 1; i <= 5; i++) {
-		let playerSelection = capitalize(prompt('choose your fighter: rock, paper or scissors', ''));
-		let computerSelection = getComputerChoice();
-		roundWinner = playRound(playerSelection, computerSelection);
+	let computerSelection = getComputerChoice();
+	roundWinner = playRound(playerSelection, computerSelection);
 
-		switch (roundWinner) {
-			case 'ties':
-				console.log(`Ties! ${playerSelection} equals ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
-				i--;
-			break;
+	switch (roundWinner) {
+		case 'ties':
+			resultsDiv.textContent = `Ties! ${playerSelection} equals ${computerSelection.toLowerCase()}. YOU ${playerScore} - MACHINE ${computerScore}`;
+		break;
 
-			case 'player':
-				playerCounter++;
-				console.log(`You win! ${playerSelection} beats ${computerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
-			break;
+		case 'player':
+			playerScore++;
+			resultsDiv.textContent = `You win! ${playerSelection} beats ${computerSelection.toLowerCase()}. YOU ${playerScore} - MACHINE ${computerScore}`;
+			roundCounter++;
+		break;
 
-			case 'computer':
-				computerCounter++;
-				console.log(`You loose! ${computerSelection} beats ${playerSelection.toLowerCase()}. YOU ${playerCounter} - MACHINE ${computerCounter}`);
-			break;
-		};
+		case 'computer':
+			computerScore++;
+			resultsDiv.textContent = `You loose! ${computerSelection} beats ${playerSelection.toLowerCase()}. YOU ${playerScore} - MACHINE ${computerScore}`;
+			roundCounter++;
+		break;
+	};
+};
+
+function endGame() {
+	let result;
+
+	if (playerScore > computerScore) {
+		result = 'YOU WIN THE GAME';
+	} else {
+		result = 'MACHINE WINS THE GAME';
 	};
 
-	if (playerCounter > computerCounter) {
-		return 'YOU WIN THE GAME';
-	} else {
-		return 'MACHINE WINS THE GAME';
+	roundCounter = 0;
+	playerScore = 0;
+	computerScore = 0;
+
+	return result;
+}
+
+function getComputerChoice() {
+	let computerChoice = Math.floor(Math.random() * 3);
+
+	switch (computerChoice) {
+		case 0:
+			return 'Rock';
+		case 1:
+			return 'Paper';
+		case 2:
+			return 'Scissors';
 	};
 };
 
